8c3463a150e259fc7025eb64be942e1738334490
Alisson Silva dos Santos <92067558+alissons-repos@users.noreply.github.com>
2024-01-05 17:36:06 -0300

    criando funções que geram tamanhos e lados randomicos para a mosca

--- app.js
+++ app.js
@@ -12,14 +12,40 @@ function ajustarTamanhoPagina() {
 }
 
 function gerarPosicaoRandomica() {
-	posicaoX = Math.floor(Math.random() * largura) - 50;
-	posicaoY = Math.floor(Math.random() * altura) - 50;
+	const [variacao] = gerarTamanhoRandomico();
+
+	posicaoX = Math.floor(Math.random() * largura) - variacao;
+	posicaoY = Math.floor(Math.random() * altura) - variacao;
 
 	// Controle para que a imagem não suma ou crie uma barra de rolagem
 	posicaoX = posicaoX < 0 ? 0 : posicaoX;
 	posicaoY = posicaoY < 0 ? 0 : posicaoY;
 
-	console.log(posicaoX, posicaoY);
+	console.log(variacao, posicaoX, posicaoY);
+}
+
+function gerarTamanhoRandomico() {
+	const tamanho = Math.floor(Math.random() * 3);
+
+	switch (tamanho) {
+		case 0:
+			return [50, 'mosca1'];
+		case 1:
+			return [75, 'mosca2'];
+		case 2:
+			return [100, 'mosca3'];
+	}
+}
+
+function gerarLadoRandomico() {
+	const lado = Math.floor(Math.random() * 2);
+
+	switch (lado) {
+		case 0:
+			return 'lado1';
+		case 1:
+			return 'lado2';
+	}
 }
 
 ajustarTamanhoPagina();
@@ -29,9 +55,10 @@ gerarPosicaoRandomica();
 body.onresize = ajustarTamanhoPagina;
 
 // Manipulação do DOM
+let [, tamanhoStyle] = gerarTamanhoRandomico();
 let mosca = document.createElement('img');
 mosca.src = './imagens/mosca.png';
-mosca.className = 'mosca';
+mosca.className = `${tamanhoStyle} ${gerarLadoRandomico()}`;
 mosca.style.left = posicaoX + 'px';
 mosca.style.top = posicaoY + 'px';
 
